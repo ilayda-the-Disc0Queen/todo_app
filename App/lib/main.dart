1e4bd0ede21f3bb8935e51f4c7fde03ed61de0f1
Ilayda <ilayda.bakare@gmail.com>
2021-09-09 16:58:27 +0100

    Add top appbar to main function

---
+++ App/lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo app',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Todo app'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      color: Colors.yellow,
+      home: SafeArea(
+        child: DefaultTabController(
+          length: 3,
+          child: new Scaffold(
+            body: TabBarView(
+              children: [
+                new Container(color: Colors.yellow),
+                new Container(color: Colors.orange),
+                new Container(color: Colors.lightGreen),
+              ],
+            ),
+            appBar: TabBar(
+              tabs: [
+                Tab(icon: new Icon(Icons.home)),
+                Tab(icon: new Icon(Icons.rss_feed)),
+                Tab(icon: new Icon(Icons.perm_identity)),
+              ],
+              labelColor: Colors.yellow,
+              unselectedLabelColor: Colors.blue,
+              indicatorSize: TabBarIndicatorSize.label,
+              indicatorPadding: EdgeInsets.all(5.0),
+              indicatorColor: Colors.red,
+            ),
+            backgroundColor: Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}
